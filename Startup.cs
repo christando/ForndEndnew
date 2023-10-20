7914954d783080ab32a1c1c42fb8af9446f957d0
Valentino Raviendra Christando <93866810+christando@users.noreply.github.com>
2023-10-20 13:29:13 +0700

    UTS FRONTEND

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+
+
+// // ConfigureServices method in Startup.cs
+// public void ConfigureServices(IServiceCollection services)
+// {
+//     // ...
+//     services.AddCors();
+// }
+
+// // Configure method in Startup.cs
+// public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+// {
+//     // ...
+//     app.UseCors(options => options
+//         .AllowAnyOrigin() // Allow requests from any origin
+//         .AllowAnyMethod() // Allow any HTTP method
+//         .AllowAnyHeader() // Allow any HTTP headers
+//     );
+//     // ...
+// }
